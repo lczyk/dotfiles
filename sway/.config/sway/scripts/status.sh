37a492760b2fbc895d417bb99459979f9a5a5acb
marcin <marcin.konow@lczyk.xyz>
2025-06-30 17:39:13 +0100

    status top and temp

--- sway/.config/sway/scripts/status.sh
+++ sway/.config/sway/scripts/status.sh
@@ -2,22 +2,22 @@
 # <short month> <numeric month>-<numeric day> <short weekday> <hour>:<minute>
 DATE=$(date +'%b %m-%d %a %H:%M')
 
-# battery
+# batte
 _upower=$(upower -i $(upower -e | grep 'battery'))
 BATTERY=$(echo "$_upower" | grep -E "percentage" | awk '{gsub(/%/,""); printf("%.0f%%",$2)}')
 BATTERY_STATE=$(echo "$_upower" | grep -E "state" | awk '{print($2)}')
 
 SHORT_STATE=""
 if [ "$BATTERY_STATE" = "charging" ]; then
-    SHORT_STATE="c"
+    SHORT_STATE="^"
 elif [ "$BATTERY_STATE" = "discharging" ]; then
-    SHORT_STATE="d"
+    SHORT_STATE="v"
 elif [ "$BATTERY_STATE" = "fully-charged" ]; then
-    SHORT_STATE="f"
+    SHORT_STATE="~"
 elif [ "$BATTERY_STATE" = "unknown" ]; then
     SHORT_STATE="?"
 else
-    SHORT_STATE="X"
+    SHORT_STATE="x"
 fi
 
 # volume
@@ -35,7 +35,13 @@ if [ -n "$VPN" ]; then
     WIFI_AND_VPN="${WIFI_AND_VPN} (${VPN})"
 fi
 
+# get the top cpu usage but ignore 'top'
+TOP=$(top -o%CPU -bn1 | head -n9 | grep -v top | tail -n1 | awk '{print($9,$12)}')
+
+# temperature
+TEMP=$(cat /sys/class/thermal/thermal_zone*/temp | sort -n | tail -n1 | awk '{printf("%.1fC",$1/1000)}')
+
 # overall status
-STATUS="${WIFI_AND_VPN} | ${BRIGHTNESS} | ${VOLUME} | ${BATTERY} ${SHORT_STATE} | ${DATE}"
+STATUS="${TOP} | ${TEMP} | ${WIFI_AND_VPN} | ${BRIGHTNESS} | ${VOLUME} | ${BATTERY} ${SHORT_STATE} | ${DATE}"
 
 echo "$STATUS "
