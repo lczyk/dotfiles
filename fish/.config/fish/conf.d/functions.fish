241b8892488d32df006d33819cd299147c960c24
marcin <marcin.konow@lczyk.xyz>
2025-07-03 13:07:30 +0100

    moved stuff in fish conf.d

--- fish/.config/fish/conf.d/functions.fish
+++ fish/.config/fish/conf.d/functions.fish
@@ -1,7 +1,15 @@
+function whats_my_ip
+    dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
+end
+
 # alias for downloading run.sh
 functions --erase runsh
 function runsh
     [ -d .vscode ] || mkdir .vscode
     [ -f .vscode/run.sh ] && mv .vscode/run.sh .vscode/run.sh~
     curl https://raw.githubusercontent.com/MarcinKonowalczyk/run_sh/master/run.sh >.vscode/run.sh && chmod u+x .vscode/run.sh
+end
+
+function test_microphone
+    arecord -vvv -f dat /dev/null
 end
--- fish/.config/fish/conf.d/test_microphone.fish
+++
@@ -1,3 +0,0 @@
-function test_microphone
-    arecord -vvv -f dat /dev/null
-end
