cb2f8856848522c2311095bbcb72d9eb3404617b
marcin <marcin.konow@lczyk.xyz>
2025-07-04 15:34:45 +0100

    remove some eza aliases

--- fish/.config/fish/conf.d/eza.fish
+++ fish/.config/fish/conf.d/eza.fish
@@ -1,7 +1,4 @@
 if type -q eza
-    set -x _EZA 'eza --group-directories-first --long --git --almost-all --show-symlinks --color=always --time-style=long-iso' 
+    set -x _EZA 'eza --group-directories-first --long --git --almost-all --show-symlinks --color=always --time-style=long-iso'
     alias ls=$_EZA
-    alias ls1="$_EZA -T -L1"
-    alias ls2="$_EZA -T -L2"
-    alias ls3="$_EZA -T -L3"
 end
