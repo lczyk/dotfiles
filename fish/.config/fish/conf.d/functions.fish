bc6a0a47afbd57f908ce05eb32c067a7162c86cc
marcin <marcin.konow@lczyk.xyz>
2025-07-04 15:34:55 +0100

    update username

--- fish/.config/fish/conf.d/functions.fish
+++ fish/.config/fish/conf.d/functions.fish
@@ -7,7 +7,7 @@ functions --erase runsh
 function runsh
     [ -d .vscode ] || mkdir .vscode
     [ -f .vscode/run.sh ] && mv .vscode/run.sh .vscode/run.sh~
-    curl https://raw.githubusercontent.com/MarcinKonowalczyk/run_sh/master/run.sh >.vscode/run.sh && chmod u+x .vscode/run.sh
+    curl https://raw.githubusercontent.com/lczyk/run_sh/master/run.sh >.vscode/run.sh && chmod u+x .vscode/run.sh
 end
 
 function test_microphone
