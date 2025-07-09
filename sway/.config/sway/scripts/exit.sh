32594f973d5f82e559c455a434cd5a18228b395f
marcin <marcin.konow@lczyk.xyz>
2025-07-09 16:55:31 +0100

    exit script

---
+++ sway/.config/sway/scripts/exit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# script by @FunctionalHacker
+# https://git.korhonen.cc/FunctionalHacker/dotfiles
+# from commit 245694fb0ed6f37f60bce4b4198a22e2cd24da07
+
+# used to exit sway with a menu
+# bindsym $mod+Shift+e exec $term --app-id fzf-launcher --login-shell /bin/sh -c "$scripts/exit.sh"
+
+RESP=$(cat <<EOF | fzf +s --tac
+Shutdown
+Reboot
+Suspend
+Lock
+Exit
+Cancel
+EOF
+);
+
+case "$RESP" in
+	Cancel)
+		exit 0
+		;;
+	Shutdown)
+		systemctl poweroff
+		;;
+	Reboot)
+		systemctl reboot
+		;;
+	Suspend)
+		systemctl suspend
+		;;
+	Lock)
+		loginctl lock-session $(loginctl show-user $USER -p Sessions | cut -d'=' -f2)
+		;;
+	Exit)
+		swaymsg exit
+		;;
+	*)
+		exit 1
+esac
