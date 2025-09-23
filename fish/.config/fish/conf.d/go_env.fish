c427f3ef321b1451321e25c91b7e01fae0839d6e
marcin <marcin.konowalczyk@canonical.com>
2025-09-23 08:29:25 +0100

    feat: manual go installation

--- fish/.config/fish/conf.d/go_env.fish
+++ fish/.config/fish/conf.d/go_env.fish
@@ -1,3 +1,9 @@
 if test -e "$HOME/go/env.fish"
     source "$HOME/go/env.fish"
 end
+
+# Maybe we installed go manually
+# Add Go to PATH if it exists
+if test -d /usr/local/go/bin
+    set -gx PATH /usr/local/go/bin $PATH
+end
