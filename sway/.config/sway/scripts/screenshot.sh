3576ed8f14cecd48177d1d993af02e10781575dc
lczyk <marcin.konowalczyk@canonical.com>
2025-12-05 14:23:04 +0000

    feat: pick color to clipboard

--- sway/.config/sway/scripts/screenshot.sh
+++ sway/.config/sway/scripts/screenshot.sh
@@ -22,11 +22,12 @@ fi
 mode="$1"
 filename="${2:-$(date +%Y-%m-%d_%H-%M-%S).png}"
 
-# TODO: fiox bash env
-# **TEMORAROLLY** HACK IN .local/bin into the path 
+# TODO: fix bash env
+# **TEMPORARILY** HACK IN .local/bin into the path 
 if ! command -v grim &> /dev/null; then
-    PATH="$PATH:$HOME/.local/bin"
-    if ! command -v grim &> /dev/null; then
+    if [ -f "$HOME/.local/bin/grim" ]; then
+        PATH="$PATH:$HOME/.local/bin"
+    else
         echo "Error: grim is not installed or not found in PATH. Please install grim."
         exit 1
     fi
@@ -39,13 +40,24 @@ case "$mode" in
     area)
         grim -g "$(slurp -d)" "$SCREENSHOT_DIR/$filename"
         ;;
+    color-clipboard)
+        position=$(slurp -p)
+        temp_file=$(mktemp --suffix=.ppm)
+        grim -t ppm -g "$position" "$temp_file"
+        # parse the ppm file to get the color of the only pixel
+        # note that the 4th line will be the RBG in *bytes* so we need to convert it to hex
+        color=$(tail -n +4 "$temp_file" | head -n 1 | od -An -t u1 | tr -s ' ' | cut -d ' ' -f 2-4 | awk '{printf("#%02x%02x%02x\n", $1, $2, $3)}')
+        echo "Picked color: $color"
+        echo -n "$color" | wl-copy
+        rm "$temp_file"
+        ;;
     window)
         # grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused? == true) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" "$SCREENSHOT_DIR/$filename"
         grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | "\(.rect.x+.window_rect.x),\(.rect.y+.window_rect.y) \(.window_rect.width)x\(.window_rect.height)"' | slurp)" "$SCREENSHOT_DIR/$filename"
         ;;
     *)
         echo "Invalid mode: $mode"
-        echo "Valid modes are: full, area, window"
+        echo "Valid modes are: full, area, window and color-clipboard"
         exit 1
         ;;
 esac
@@ -53,6 +65,5 @@ esac
 # the recent screenshot is always saved as recent.png
 if [ -f "$SCREENSHOT_DIR/$filename" ]; then
     cp "$SCREENSHOT_DIR/$filename" "$SCREENSHOT_DIR/recent.png"
+    echo "Screenshot saved to $SCREENSHOT_DIR/$filename"
 fi
-
-echo "Screenshot saved to $SCREENSHOT_DIR/$filename"
