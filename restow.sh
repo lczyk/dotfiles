9754c0ee15f4fab95a31a40687c5d552c4667224
lczyk <marcin.konowalczyk@canonical.com>
2025-12-24 10:03:07 +0000

    feat: filter bin

--- restow.sh
+++ restow.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
+#spellchecker: ignore restow Restowed
 
-# ALL is an array of all the directories in the current directory
-ALL=$(find . -maxdepth 1 -type d ! -name '.*' | sed 's|^\./||' | sort)
+function main() {
+    local all=$(
+        find . -maxdepth 1 -type d \
+            ! -name '.*' \
+            ! -name 'bin' \
+        | sed 's|^\./||' \
+        | sort
+    )
 
-for dir in $ALL; do
-    if [ -d "$dir" ]; then
+    for dir in $all; do
         stow --restow --target="$HOME" "$dir" 2>/dev/null
         if [ $? -ne 0 ]; then
             echo "Failed to restow $dir"
         else
             echo "Restowed $dir"
         fi
-    fi
-done
+    done
+}
+
+main "$@"
