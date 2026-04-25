8f66f4c60d955bfe2f62934e71ea71a4f2ae3073
marcin <marcin.konow@lczyk.xyz>
2026-04-25 16:23:33 +0100

    chore: typo

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,5 +1,5 @@
 if type -q fzf
-    # TODO: fzf shell integration does not work in onlder versions of fzf
+    # TODO: fzf shell integration does not work in older versions of fzf
     # see: https://github.com/junegunn/fzf?tab=readme-ov-file#installation
     fzf --fish 2>/dev/null | source
     export FZF_DEFAULT_OPTS='--multi --exact --cycle --height 40% --layout reverse --border top'
