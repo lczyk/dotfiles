6c3426adf6d987a0edc2d5bd7f7a37e6699131ff
lczyk <marcin.konowalczyk@canonical.com>
2026-06-26 09:13:45 +0100

    refactor: ruby gem path via GEM_HOME from Gem.user_dir

--- stow/x1/fish/.config/fish/conf.d/ruby.fish
+++ stow/x1/fish/.config/fish/conf.d/ruby.fish
@@ -1,6 +1,5 @@
-# x1-only: add ruby gem bin dirs under ~/.local/share/gem/ruby/*/bin to PATH.
-for dir in (find $HOME/.local/share/gem/ruby -maxdepth 2 -type d -name bin 2>/dev/null)
-    if not contains -- $dir $PATH
-        set -gx PATH $dir $PATH
-    end
+if type -q ruby
+    set -gx GEM_HOME (ruby -e 'puts Gem.user_dir')
+    set -gx GEM_PATH $GEM_HOME
+    fish_add_path $GEM_HOME/bin
 end
