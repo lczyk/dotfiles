c1c4d84c72b5206692a59acc51b1641957ac246e
lczyk <marcin.konow@lczyk.xyz>
2026-08-07 08:16:30 +0100

    perf(fish): skip eza --git status on huge repos
    git-dir index size over 5mb (e.g. fil-c) makes eza's per-file git
    status scan slow. check index size via stat before adding --git.

--- stow/common/fish/.config/fish/conf.d/eza.fish
+++ stow/common/fish/.config/fish/conf.d/eza.fish
@@ -1,4 +1,13 @@
 if type -q eza
-    set -x _EZA 'eza --group-directories-first --long --git --almost-all --show-symlinks --color=always --time-style=long-iso'
-    alias ls=$_EZA
+    function ls --description 'eza-backed ls, skips --git status on huge repos'
+        set -l flags --group-directories-first --long --almost-all --show-symlinks --color=always --time-style=long-iso
+        set -l gitdir (command git rev-parse --git-dir 2>/dev/null)
+        if test -n "$gitdir"
+            set -l idx_size (stat -f%z "$gitdir/index" 2>/dev/null)
+            if test -z "$idx_size" -o "$idx_size" -lt 5000000
+                set flags $flags --git
+            end
+        end
+        eza $flags $argv
+    end
 end
